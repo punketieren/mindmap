5d449a1594454a75c150877d1f8b988922c898e9
punketieren <pasha.ustal@gmail.com>
2026-05-30 05:16:01 +0300

    Update init-prosemirror.js

--- init-prosemirror.js
+++ init-prosemirror.js
@@ -38,7 +38,7 @@
     }
     
     try {
-        const response = await fetch('/printery/mindmap/to-do.md');
+        const response = await fetch('/mindmap/to-do.md');
         if (!response.ok) {
             throw new Error(`HTTP ${response.status}`);
         }
